0983e89e50c856307ef927c9c6bf700bb2de1435
Lee-na-yun <happyin3333@nate.com>
2022-11-22 10:39:43 +0900

    ui 메서드로 빼기

--- lib/view/pages/chat_room/chat_room_page.dart
+++ lib/view/pages/chat_room/chat_room_page.dart
@@ -7,14 +7,18 @@ class ChatRoomPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
-      body: ListView.separated(
-        itemCount: 2,
-        itemBuilder: (context, index) => ListTile(
-          title: Text("msg:안녕"),
-          subtitle: Text("from:ssar"),
-        ),
-        separatorBuilder: (context, index) => Divider(),
+      body: _buildListView(),
+    );
+  }
+
+  ListView _buildListView() {
+    return ListView.separated(
+      itemCount: 2,
+      itemBuilder: (context, index) => ListTile(
+        title: Text("msg:안녕"),
+        subtitle: Text("from:ssar"),
       ),
+      separatorBuilder: (context, index) => Divider(),
     );
   }
 
