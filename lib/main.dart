864ec416fea9d0bfdcaa3814c6860b8d37113473
Lee-na-yun <happyin3333@nate.com>
2022-11-22 12:18:16 +0900

    flutter에 firebase 설정 ã완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_firestore_stream/firebase_options.dart';
 import 'package:riverpod_firestore_stream/view/pages/chat_room/chat_room_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //메인메서드에 비동기 작업이 있으면 꼭 필요!!
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     // For widgets to be able to read providers, we need to wrap the entire
     // application in a "ProviderScope" widget.
